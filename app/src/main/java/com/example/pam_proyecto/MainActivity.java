f55f70ab1e24792402e8db4b04712039316ebfab
Ccanochu <ccanochu@gmail.com>
2021-05-27 18:26:55 -0500

    CANO cambios adaptados a los bocetos (fase de prototipo)

--- app/src/main/java/com/example/pam_proyecto/MainActivity.java
+++ app/src/main/java/com/example/pam_proyecto/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.pam_proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +12,9 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     EditText tvt_correo, tvt_contraseña;
     Button Ingresar;
+    FragmentTransaction transaction;
+    Fragment fragmentInicio, registrarFragment, salasFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +23,27 @@ public class MainActivity extends AppCompatActivity {
 
         tvt_correo = findViewById(R.id.tvt_correo);
         tvt_contraseña = findViewById(R.id.tvt_contraseña);
+        fragmentInicio = new InicioFragment();
+        registrarFragment = new RegistrarFragment();
+        salasFragment = new SalasFragment();
+
+        //invocar nuestro fragmento principal
+        //getSupportFragmentManager().beginTransaction().add(R.id.contenedorFragments,fragmentInicio).commit();
+
     }
     public void Ingresar(View v){
+        /*transaction = getSupportFragmentManager().beginTransaction();
+        switch (v.getId()){
+            case R.id.btnNuevo: transaction.replace(R.id.contenedorFragments,registrarFragment);
+                transaction.addToBackStack(null);
+                break;
 
+            case R.id.btnIngresar:transaction.replace(R.id.contenedorFragments,salasFragment);
+                transaction.addToBackStack(null);
+                break;
+
+
+        }
+        transaction.commit();*/
     }
 }
