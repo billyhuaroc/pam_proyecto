26d6aaf73f4e38fc2a00c2a7fecb384ee33f1161
Ccanochu <ccanochu@gmail.com>
2021-05-27 20:45:40 -0500

    CANO iniciarsesion

--- app/src/main/java/com/example/pam_proyecto/MainActivity.java
+++ app/src/main/java/com/example/pam_proyecto/MainActivity.java
@@ -10,10 +10,6 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    EditText tvt_correo, tvt_contraseña;
-    Button Ingresar;
-    FragmentTransaction transaction;
-    Fragment fragmentInicio, registrarFragment, salasFragment;
 
 
     @Override
@@ -21,29 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvt_correo = findViewById(R.id.tvt_correo);
-        tvt_contraseña = findViewById(R.id.tvt_contraseña);
-        fragmentInicio = new InicioFragment();
-        registrarFragment = new RegistrarFragment();
-        salasFragment = new SalasFragment();
 
-        //invocar nuestro fragmento principal
-        //getSupportFragmentManager().beginTransaction().add(R.id.contenedorFragments,fragmentInicio).commit();
-
-    }
-    public void Ingresar(View v){
-        /*transaction = getSupportFragmentManager().beginTransaction();
-        switch (v.getId()){
-            case R.id.btnNuevo: transaction.replace(R.id.contenedorFragments,registrarFragment);
-                transaction.addToBackStack(null);
-                break;
-
-            case R.id.btnIngresar:transaction.replace(R.id.contenedorFragments,salasFragment);
-                transaction.addToBackStack(null);
-                break;
-
-
-        }
-        transaction.commit();*/
     }
 }
