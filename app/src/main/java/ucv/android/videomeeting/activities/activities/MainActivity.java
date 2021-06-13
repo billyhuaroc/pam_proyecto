14d08f266eb0e48b523d25b65b4a4d9df545416d
Ccanochu <ccanochu@gmail.com>
2021-06-13 18:21:56 -0500

    display users

--- app/src/main/java/ucv/android/videomeeting/activities/activities/MainActivity.java
+++ app/src/main/java/ucv/android/videomeeting/activities/activities/MainActivity.java
@@ -1,16 +1,85 @@
 package ucv.android.videomeeting.activities.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
 
 import ucv.android.videomeeting.R;
+import ucv.android.videomeeting.activities.utilities.Constants;
+import ucv.android.videomeeting.activities.utilities.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        preferenceManager=new PreferenceManager(getApplicationContext());
+
+        TextView textTittle = findViewById(R.id.textTittle);
+        textTittle.setText(String.format(
+            "%s %s",
+            preferenceManager.getString(Constants.KEY_NOMBRE),
+            preferenceManager.getString(Constants.KEY_CARGO)
+        ));
+
+        findViewById(R.id.textSignOut).setOnClickListener(v -> SingOut());
+
+
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
+            @Override
+            public void onComplete(@NonNull @NotNull Task<String> task) {
+                if(task.isSuccessful() && task.getResult()!=null) {
+                    sendFCMTokenToDataBase(task.getResult());
+                }
+            }
+        });
+    }
+
+    private void sendFCMTokenToDataBase(String token){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference =
+            database.collection(Constants.KEY_COLLECION_USERS).document(
+                    preferenceManager.getString(Constants.KEY_USER_ID)
+            );
+        documentReference.update(Constants.KEY_FCM_TOKEN,token)
+            .addOnCompleteListener(task -> Toast.makeText(MainActivity.this,"Token actualizado satisfactoriamente", Toast.LENGTH_SHORT).show())
+            .addOnFailureListener(e -> Toast.makeText(MainActivity.this,"Token no enviado!: "+e.getMessage(), Toast.LENGTH_SHORT).show());
+
+    }
+
+    private void SingOut(){
+        Toast.makeText(this,"Cerrando sesión...",Toast.LENGTH_SHORT).show();
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference=
+            database.collection(Constants.KEY_COLLECION_USERS).document(
+                    preferenceManager.getString(Constants.KEY_USER_ID)
+            );
+        HashMap<String, Object> updates=new HashMap<>();
+        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete());
+        documentReference.update(updates)
+                .addOnSuccessListener(unused -> {
+                    startActivity(new Intent(getApplicationContext(),IniciarSesion.class));
+                    finish();
+                })
+                .addOnFailureListener(e -> Toast.makeText(MainActivity.this, "No pudo cerrar sesión", Toast.LENGTH_SHORT).show());
     }
 }
