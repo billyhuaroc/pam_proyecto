ca74fdf35a4fadcc85f2864caa4fde731ccd1f4a
Ccanochu <ccanochu@gmail.com>
2021-06-15 02:31:01 -0500

    display user list

--- app/src/main/java/ucv/android/videomeeting/activities/activities/MainActivity.java
+++ app/src/main/java/ucv/android/videomeeting/activities/activities/MainActivity.java
@@ -2,9 +2,12 @@ package ucv.android.videomeeting.activities.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,19 +16,29 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import ucv.android.videomeeting.R;
+import ucv.android.videomeeting.activities.adapters.UserAdapter;
+import ucv.android.videomeeting.activities.models.Usuario;
 import ucv.android.videomeeting.activities.utilities.Constants;
 import ucv.android.videomeeting.activities.utilities.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
 
     private PreferenceManager preferenceManager;
+    private List<Usuario> usuarios;
+    private UserAdapter userAdapter;
+    private TextView textErrorMessage;
+    private ProgressBar usersProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +65,56 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        RecyclerView userRecyclerView = findViewById(R.id.usersRecyclerView);
+        textErrorMessage = findViewById(R.id.textErrorMessage);
+        usersProgressBar = findViewById(R.id.usersProgressBar);
+
+        usuarios=new ArrayList<>();
+        userAdapter = new UserAdapter(usuarios);
+        userRecyclerView.setAdapter(userAdapter);
+
+        getUsers();
+    }
+
+    private void getUsers(){
+        usersProgressBar.setVisibility(View.VISIBLE);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECION_USERS)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<QuerySnapshot> task) {
+                        usersProgressBar.setVisibility(View.GONE);
+                        String miUserId = preferenceManager.getString(Constants.KEY_USER_ID);
+                        if (task.isSuccessful() && task.getResult()!=null){
+                            for (QueryDocumentSnapshot documentSnapshot : task.getResult()){
+                                if (miUserId.equals(documentSnapshot.getId())){
+                                    //aquí, mostraremos la lista de usuarios excepto el usuario
+                                    // que ha iniciado sesión actualmente, porque nadie se reunirá
+                                    //con él mismo. es por eso que excluimos de la lista a
+                                    // un usuario que inició sesión.
+                                    continue;
+                                }
+                                Usuario usuario = new Usuario();
+                                usuario.nombre = documentSnapshot.getString(Constants.KEY_NOMBRE);
+                                usuario.cargo = documentSnapshot.getString(Constants.KEY_CARGO);
+                                usuario.email = documentSnapshot.getString(Constants.KEY_EMAIL);
+                                usuario.token = documentSnapshot.getString(Constants.KEY_FCM_TOKEN);
+                                usuarios.add(usuario);
+                            }
+                            if (usuarios.size()>0){
+                                userAdapter.notifyDataSetChanged();
+                            }else {
+                                textErrorMessage.setText(String.format("%s","Usuarios no disponibles"));
+                                textErrorMessage.setVisibility(View.VISIBLE);
+                            }
+                        }else{
+                            textErrorMessage.setText(String.format("%s","Usuarios no disponibles"));
+                            textErrorMessage.setVisibility(View.VISIBLE);
+                        }
+                    }
+                });
     }
 
     private void sendFCMTokenToDataBase(String token){
@@ -61,7 +124,8 @@ public class MainActivity extends AppCompatActivity {
                     preferenceManager.getString(Constants.KEY_USER_ID)
             );
         documentReference.update(Constants.KEY_FCM_TOKEN,token)
-            .addOnCompleteListener(task -> Toast.makeText(MainActivity.this,"Token actualizado satisfactoriamente", Toast.LENGTH_SHORT).show())
+                //ya no necesitamos este mensaje de "token actualizado correctamente", así que elimínelo
+            //.addOnCompleteListener(task -> Toast.makeText(MainActivity.this,"Token actualizado satisfactoriamente", Toast.LENGTH_SHORT).show())
             .addOnFailureListener(e -> Toast.makeText(MainActivity.this,"Token no enviado!: "+e.getMessage(), Toast.LENGTH_SHORT).show());
 
     }
