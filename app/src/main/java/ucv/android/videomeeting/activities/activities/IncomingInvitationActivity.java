fc84ebcfef30601fb5ae41340495a29fa6cd2f34
Ccanochu <ccanochu@gmail.com>
2021-06-17 15:16:41 -0500

    outcoming invitation meeting

---
+++ app/src/main/java/ucv/android/videomeeting/activities/activities/IncomingInvitationActivity.java
@@ -0,0 +1,16 @@
+package ucv.android.videomeeting.activities.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ucv.android.videomeeting.R;
+
+public class IncomingInvitationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_incoming_invitation);
+    }
+}
