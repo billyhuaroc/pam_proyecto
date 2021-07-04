aa2f7a313533af7621c8220a02c1a78055d977ae
Ccanochu <ccanochu@gmail.com>
2021-07-04 13:10:00 -0500

    hola mundo

--- app/src/main/java/ucv/android/videomeeting/activities/models/Usuario.java
+++
@@ -1,7 +0,0 @@
-package ucv.android.videomeeting.activities.models;
-
-import java.io.Serializable;
-
-public class Usuario implements Serializable {
-    public String nombre, cargo,email,token;
-}
